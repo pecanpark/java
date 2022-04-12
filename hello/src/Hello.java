c5e5c15882f35026a2c48be25695f7ab769365ac
pecanpark <99879255+pecanpark@users.noreply.github.com>
2022-04-12 09:50:05 +0900

    hello world

---
+++ hello/src/Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("반갑습니다. 자바 프로그래머 여러분!!");
+		System.out.println("잘 지내시나요?");
+		System.out.println("네. 잘 지냅니다.");
+	}
+
+}
